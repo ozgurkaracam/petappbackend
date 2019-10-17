95a111cd7373fd16db48c5baf85e0ba30eeea49f
ozgurkaracam <ozgurkrcm@hotmail.com>
2019-10-17 10:08:35 +0300

    duyuru eklendi.

---
+++ models/Duyuru.js
@@ -0,0 +1,10 @@
+const mongoose=require('mongoose');
+const Schema=mongoose.Schema;
+
+var DuyuruSchema=new Schema({
+    duyurutext:String,
+    duyururesim:{type:String, default:""},
+    duyurutarih:{type:Date, default:Date.now()}
+});
+
+module.exports=mongoose.model("duyuru",DuyuruSchema);
