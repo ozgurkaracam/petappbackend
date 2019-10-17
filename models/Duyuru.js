f568c8428c34f6eb4729d192d08d14b6fb63812c
ozgurkaracam <ozgurkrcm@hotmail.com>
2019-10-17 16:11:04 +0300

    duyuru added.

--- models/Duyuru.js
+++ models/Duyuru.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 const mongoose=require('mongoose');
 const Schema=mongoose.Schema;
@@ -9,15 +8,4 @@ var DuyuruSchema=new Schema({
     duyurutarih:{type:Date, default:Date.now()}
 });
 
-=======
-const mongoose=require('mongoose');
-const Schema=mongoose.Schema;
-
-var DuyuruSchema=new Schema({
-    duyurutext:String,
-    duyururesim:{type:String, default:""},
-    duyurutarih:{type:Date, default:Date.now()}
-});
-
->>>>>>> 95a111cd7373fd16db48c5baf85e0ba30eeea49f
 module.exports=mongoose.model("duyuru",DuyuruSchema);
--- routes/duyuru.js
+++ routes/duyuru.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express=require('express');
 const router=express.Router();
 
@@ -15,24 +14,4 @@ router.post('/',(req,res)=>{
         err ? res.json(err) : res.json(data);
     });
 });
-
-=======
-const express=require('express');
-const router=express.Router();
-
-const Duyuru=require("../models/Duyuru");
-
-router.get('/',(req,res)=>{
-    Duyuru.find({},(err,data)=>{
-        err ? res.json(err) : res.json(data);
-    });
-});
-
-router.post('/',(req,res)=>{
-    Duyuru.insertMany(req.body,(err,data)=>{
-        err ? res.json(err) : res.json(data);
-    });
-});
-
->>>>>>> 95a111cd7373fd16db48c5baf85e0ba30eeea49f
 module.exports=router;
