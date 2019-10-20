9148f52eb14eb71efdcc9e5f03494654f0a6d2bf
ozgurkaracam <ozgurkrcm@hotmail.com>
2019-10-20 11:48:20 +0300

    date format & duyuru düzenlemesi.

--- routes/duyuru.js
+++ routes/duyuru.js
@@ -1,12 +1,25 @@
 const express=require('express');
 const router=express.Router();
+var dateformat=require('date-format');
 
 const Duyuru=require("../models/Duyuru");
 
 router.get('/',(req,res)=>{
+    var list=[]
     Duyuru.find({},(err,data)=>{
-        err ? res.json(err) : res.json(data);
-    });
+        if(err)
+            throw err;
+        data.forEach((item,index)=>{
+            list.push({
+                'duyururesim':item.duyururesim,
+                'duyurutarih':dateformat.asString('dd/MM/yyyy hh:mm',item.duyurutarih),
+                '_id':item._id,
+                'duyurutext':item.duyurutext,
+                '__V':item.__v
+            });
+        });
+        res.json(list);
+    }).sort({'duyurutarih':-1});
 });
 
 router.post('/',(req,res)=>{
