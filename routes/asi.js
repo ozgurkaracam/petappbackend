753506ddb61062a57976772a5598a4dfbc91a18a
ozgurkaracam <ozgurkrcm@hotmail.com>
2019-10-22 09:37:42 +0300

    gecmis, gelecek asilar.

--- routes/asi.js
+++ routes/asi.js
@@ -1,5 +1,6 @@
 const express=require('express');
 const router=express.Router();
+var format=require('date-format');
 
 var Asi=require('../models/Asi');
 
@@ -30,6 +31,50 @@ router.get('/sahip/:id',(req,res,next)=>{
             var asilar=[];
             data.forEach((item,index)=>{
                if(req.params.id==item.pet.sahipid){
+                   asilar.push(item);
+               }
+            });
+            
+            res.json(asilar);
+        }
+            
+    }).populate('pet');
+});
+
+router.get('/sahip/:id/gecmis',(req,res,next)=>{
+    
+    Asi.find({},(err,data)=>{
+        if(err)
+            res.json(err);
+        else{
+            var asilar=[];
+            data.forEach((item,index)=>{
+                
+                var tarih=format.parse("dd/MM/yyyy", item.tarih).getTime();
+                var bugun=Date.now();
+               if(req.params.id==item.pet.sahipid && tarih<=bugun){
+                   asilar.push(item);
+               }
+            });
+            
+            res.json(asilar);
+        }
+            
+    }).populate('pet');
+});
+
+router.get('/sahip/:id/gelecek',(req,res,next)=>{
+    
+    Asi.find({},(err,data)=>{
+        if(err)
+            res.json(err);
+        else{
+            var asilar=[];
+            data.forEach((item,index)=>{
+                
+                var tarih=format.parse("dd/MM/yyyy", item.tarih).getTime();
+                var bugun=Date.now();
+               if(req.params.id==item.pet.sahipid && tarih>bugun){
                    asilar.push(item);
                }
             });
